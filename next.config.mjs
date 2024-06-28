7037f1f58727400175c59f2d696eb1f05f48f017
Rishabh Ratan <rishabhkarondiya@gmail.com>
2024-06-28 19:18:46 +0530

    git lowercase value ignored file changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                hostname:'utfs.io'
-            }
-        ]
-    }
-};
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
+  },
+}
 
 export default nextConfig;
